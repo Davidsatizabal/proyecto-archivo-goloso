0835ecd417a972b3b5580d4ef7c21a6963fcadf9
David <davidsatizabal705@gmail.com>
2025-12-19 08:42:50 -0500

    Renombrar productos a datos

--- vistas/datos.php
+++ vistas/datos.php
@@ -19,6 +19,24 @@
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg12">
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Criterios de Búsqueda</h3>
+                <div class="card-tools">
+                  <div class="button">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" id="btnLimpiarBusqueda">
+                      <i class="fas fa-minus"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         
       </div><!-- /.container-fluid -->
     </div>
