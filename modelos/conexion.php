62bd2d697aa36804d4d37747caaa549a9734b20b
David <davidsatizabal705@gmail.com>
2025-12-19 11:37:04 -0500

    nueva conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,7 +5,7 @@ class Conexion
   static public function conectar(){
    try
    {
-    $conn = new PDO("mysql:host=localhost;dbname=market-pos", "root", "",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn = new PDO("mysql:host=localhost;proyecto-archivo-goloso", "root", "",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $conn->exec("set names utf8");
     return $conn;
    }
